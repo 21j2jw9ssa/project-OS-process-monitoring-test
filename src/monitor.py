4ad912a4922c253f29e642c9767de652597e6582
侯柏瑞 <121330378+21j2jw9ssa@users.noreply.github.com>
2026-01-28 18:15:57 +0800

    Refactor monitoring functions and improve logging

--- src/monitor.py
+++ src/monitor.py
@@ -11,6 +11,7 @@ import os          # To detect system resources
 import psutil      # necessary for system monitoring, particularly for industrial purposes
 import time        # for monitoring for a fixed period
 import datetime
+import math
 from pathlib import Path
 
 # Base directory to save a log file
@@ -19,13 +20,6 @@ DATA_DIR = BASE_DIR / "logs"
 
 # Create a directory similar to "./../logs" if not exist
 DATA_DIR.mkdir(exist_ok=True)
-
-INTERVAL = 59.7    # time gap between each monitoring, in seconds
-
-# Threshold values to demonstrate system warnings if passed
-THRESHOLD_CPU = 70.0        # as percent (%)
-THRESHOLD_MEM = 500.0       # as MB
-THRESHOLD_RUNTIME = 3600    # seconds
 
 # Fields to draw a table sheet
 FIELDS = [
@@ -53,7 +47,7 @@ def Process(pid:int, name:str, cpu_usage, memory_usage, runtime:int):
   :type cpu_usage: float
   :param cpu_usage: how much CPU has been consumed by this process, in percent (%)
 
-  :type cpu_usage: float
+  :type memory_usage: float
   :param memory_usage: how much memory has been occupied by this process, in MB
 
   :type runtime: int
@@ -90,7 +84,7 @@ def PrintAndLog(str :str="", end :str="\n", file :str="OS_process_monitoring-log
 
 
 
-def apply_format(value, template):
+def dataFormat(value, template):
   """
   use for formatting values in strings
   
@@ -103,7 +97,7 @@ def apply_format(value, template):
 
 
 
-def drawSeparationLine(fields, file):
+def drawSeparationLine(fields):
   """
   How length of a horizontal border in sheets is calculated:
   - Calculate how many spaces all headers' names uses.
@@ -130,29 +124,31 @@ def drawSeparationLine(fields, file):
   line = ""
   for field in fields:
     line += f"+{ "-" * ( 1 + field['column_width'] + 1 ) }"
-  line += "+"
-  PrintAndLog( line, file=file )
+  line += "+\n"
+  return line
 
 
 
 
 
-def logHeader(fields, file):
+def logHeader(fields):
   """
   Print fields and their names in a table sheet
   
   :param fields: a field tuple
   :param file: file to write headers to
   """
+  fieldCtnt = ""
   for field in fields:
-    PrintAndLog( f"| { field['name']:<{ field['column_width'] }}", end=" ", file=file )
-  PrintAndLog( "|", file=file )
+    fieldCtnt += f"| { field['name']:<{ field['column_width'] }} "
+  fieldCtnt += "|\n"
+  return fieldCtnt
 
 
 
 
 
-def logRowInfo(fields, row, file):
+def logRowInfo(fields, row):
   """
   Docstring for logRowInfo
   
@@ -160,56 +156,61 @@ def logRowInfo(fields, row, file):
   :param row: a row of data
   :param file: file to write the row info to
   """
-  PrintAndLog("| ", end="", file=file)
+  rowCtnt = ""
 
   # print the record in different field values
   for i in range( len(FIELDS) ):
-    if FIELDS[i]["align"] == "right":
-      PrintAndLog( f"{apply_format( row[ FIELDS[i]["name"] ], fields[i]['format'] ):>{fields[i]['column_width']}}", end=" | ",  file=file )
+    curCol = FIELDS[i]
+    attrData = fields[i]
+    if curCol["align"] == "right":
+      rowCtnt += f"| {dataFormat( row[ curCol["name"] ], attrData['format'] ):>{attrData['column_width']}} "
     else:
-      PrintAndLog( f"{apply_format( row[ FIELDS[i]["name"] ], fields[i]['format'] ):<{fields[i]['column_width']}}", end=" | ",  file=file )
-  PrintAndLog("", file=file)
+      rowCtnt += f"| {dataFormat( row[ curCol["name"] ], attrData['format'] ):<{attrData['column_width']}} "
+  rowCtnt += "|\n"
+  return rowCtnt
 
 
 
 
 
-def LogProcessTable(rows, file):
+def LogProcessTable(rows):
   """
   Print a table of processes
   """
   if len(rows) == 0:
-    return
-  for f in FIELDS:
+    return ""
 
+  for f in FIELDS:
     # how many spaces the unit occupies in the table. Set to zero by default unless specified
     unitOccupySpaces = 0
-
-    if f["name"] == "cpu_usage":      # uses percent (%), occupies 2 spaces
+    fieldName = f["name"]
+    if fieldName == "cpu_usage":      # uses percent (%), occupies 2 spaces
       unitOccupySpaces = 2
-    elif f["name"] == "memory_usage": # uses megabytes (Windows' MB), occupies 3 spaces
+    elif fieldName == "memory_usage": # uses megabytes (Windows' MB), occupies 3 spaces
       unitOccupySpaces = 3
-    elif f["name"] == "runtime":      # uses seconds (sec.), occupies 5 spaces
+    elif fieldName == "runtime":      # uses seconds (sec.), occupies 5 spaces
       unitOccupySpaces = 5
 
     # Find the value of a specific field that occupies the most spaces...
-    maxValueLength = max( [ len( str( r[ f["name"] ] ) ) + unitOccupySpaces for r in rows ] )
+    maxValueLength = max( [ len( str( r[ fieldName ] ) ) + unitOccupySpaces for r in rows ] )
 
     # ...then find the value of the header that occupies the most spaces.
-    headerValueLength = len( f["name"] )
+    headerValueLength = len( fieldName )
 
     # Finally, set the more spacious one as the column width of the current field
     f["column_width"] = max( maxValueLength, headerValueLength )
 
+  tableCtnt = ""
+
   # Draw headers of the sheet
-  drawSeparationLine( FIELDS, file )
-  logHeader( FIELDS, file )
-  drawSeparationLine( FIELDS, file )
+  tableCtnt += drawSeparationLine(FIELDS) + logHeader(FIELDS) + drawSeparationLine(FIELDS)
 
   # Draw all rows of the sheet
   for i in range( len(rows) ):
-    logRowInfo( FIELDS, rows[i], file )
-  drawSeparationLine( FIELDS, file )
+    tableCtnt += logRowInfo( FIELDS, rows[i] )
+
+  tableCtnt += drawSeparationLine( FIELDS )
+  return tableCtnt
 
 
 
@@ -263,26 +264,36 @@ def getSnapshot( curTimeInSec: int | float, procList ):
 
 
 
-def format_duration(secs):
+def duration_readable_format(secs: int):
   """
   Converts a number of seconds into a readable string in a human-readable format,
   such as "10 hours, 20 minutes and 30 seconds".
   """
-  nHrs :int = 0
-  nMins:int = 0
-  nSecs:int = secs
-  while nSecs >= 3600:
-    nSecs -= 3600
-    nHrs += 1
-  while nSecs >= 60:
-    nSecs -= 60
-    nMins += 1
-  hourInfo = f"{ "1 hour" if nHrs == 1 else ( "" if nHrs == 0 else f"{nHrs} hours" ) }"
+  if ( not isinstance(secs, int) ) or secs < 0:
+    raise Exception( f"Number of seconds must be a natural number, but received {secs}" )
+  finTimeInfo :str = ""
+
+  ( nMins, nSecs )  = divmod(secs, 60)
+  ( nHrs, nMins )   = divmod(nMins, 60)
+  ( nDays, nHrs )   = divmod(nHrs, 24)
+  ( nWeeks, nDays ) = divmod(nDays, 7)
+
+  weekInfo = f"{ "1 week" if nWeeks == 1 else ( "" if nWeeks == 0 else f"{nWeeks} weeks" ) }"
+  dayInfo = f"{ "1 day" if nDays == 1 else ( "" if nDays == 0 else f"{nDays} days" ) }"
+  hrInfo = f"{ "1 hour" if nHrs == 1 else ( "" if nHrs == 0 else f"{nHrs} hours" ) }"
   minInfo =  f"{ "1 minute" if nMins == 1 else ( "" if nMins == 0 else f"{nMins} minutes" ) }"
-  secInfo =  f"{ "1 second" if nSecs == 1 else ( "" if nSecs == 0 else f"{nSecs} seconds" ) }"
-  hrMinConcat = f"{ ( ( ( ", " if nSecs > 0 else " and " ) if nMins > 0 else "" ) if nMins > 0 or nSecs > 0 else "" ) if nHrs > 0 else "" }"
-  minSecConcat = f"{ ( ( " and " if nHrs > 0 or nMins > 0 else "" ) if nSecs > 0 else "" ) }"
-  return f"{hourInfo}{hrMinConcat}{minInfo}{minSecConcat}{secInfo}" if nHrs > 0 or nMins > 0 or nSecs > 0 else "0 seconds"
+  secInfo = f"{ "1 second" if nSecs == 1 else ( "" if ( nSecs == 0 and ( nWeeks > 0 or nDays > 0 or nHrs > 0 or nMins > 0 ) ) else f"{nSecs} seconds" ) }"
+
+  validTimeInfo = [ t for t in [ weekInfo, dayInfo, hrInfo, minInfo, secInfo ] if t != "" ]
+
+  for i in range( len(validTimeInfo) ):
+    finTimeInfo +=validTimeInfo[i]
+    if i + 2 < len(validTimeInfo):
+      finTimeInfo += ", "
+    elif i + 2 == len(validTimeInfo):
+      finTimeInfo += " and "
+
+  return finTimeInfo
 
 
 
@@ -302,58 +313,73 @@ def clearScreen():
 
 
 
-def OS_monitoring_summary( recTime, recProcs, cpuFaultProcs, memFaultProcs, runTooLongProcs, file ):
+def OS_monitoring_summary( recTime,
+                           recProcs: int | float,
+                           cpuFaultProcs,
+                           memFaultProcs,
+                           runTooLongProcs ):
   """
   Print a summary of the OS monitoring results.
 
   Includes:
   - all monitored processes (as a table sheet)
   - all problematic processes (if any)
-    - processes with excessive CPU usage (over 70 % by default)
+    - processes with excessive CPU usage (over 70% by default)
     - processes with excessive memory usage (over 500 MB by default)
     - processes running too long (over an hour by default)
     - suggestions to resolve the problems
   """
+
+  # Threshold values to show system warnings if passed
+  THRESHOLD_CPU = 70.0        # as percent (%)
+  THRESHOLD_MEM = 500.0       # as MB
+  THRESHOLD_RUNTIME = 3600    # seconds
+
+  resultCtnt = ""
+
   if len(recProcs) == 0:
-    PrintAndLog( f"No processes found monitored", file=file )
-    return
-  PrintAndLog( f"Monitored processes recorded\n{ recTime }\ncan be seen in the following table.\n", file=file )
-  LogProcessTable( recProcs, file=file )
+    return f"No filtered processes detected { recTime }\n"
+
+  resultCtnt += f"Monitored processes recorded { recTime }\ncan be seen in the following table.\n\n"
+  resultCtnt += LogProcessTable(recProcs)
 
   # Display all the problems among the filtered, monitored processes
   if len(cpuFaultProcs) > 0 or len(memFaultProcs) > 0 or len(runTooLongProcs) > 0:
-    PrintAndLog( "\nBeware: ", file=file )
+    resultCtnt += "\nBeware: "
     if cpuFaultProcs:
-      PrintAndLog( f"{ len(cpuFaultProcs) } { "PROCESS" if len(cpuFaultProcs) == 1 else "PROCESSES" }", end="", file=file )
-      PrintAndLog( f"WITH HIGH CPU USAGE (OVER {THRESHOLD_CPU} %) DETECTED:", file=file )
-      LogProcessTable(cpuFaultProcs, file=file)
-      PrintAndLog("Suggestion:", file=file)
-      PrintAndLog("1. Restart your computer/laptop to clear all running processes", file=file)
-      PrintAndLog("2. End/restart processes that consume too much CPU", file=file)
-      PrintAndLog("3. Update drivers to enhance CPU efficiency", file=file)
-      PrintAndLog("4. Scan for malwares or other harmful softwares\n", file=file)
+      resultCtnt += f"{ len(cpuFaultProcs) } { "PROCESS" if len(cpuFaultProcs) == 1 else "PROCESSES" }\n"
+      resultCtnt += f"WITH HIGH CPU USAGE (OVER {THRESHOLD_CPU} %) DETECTED:\n"
+      resultCtnt += LogProcessTable(cpuFaultProcs)
+      resultCtnt += "Suggestion:\n"
+      resultCtnt += "1. Restart your computer/laptop to clear all running processes\n"
+      resultCtnt += "2. End/restart processes that consume too much CPU\n"
+      resultCtnt += "3. Update drivers to enhance CPU efficiency\n"
+      resultCtnt += "4. Scan for malwares or other harmful softwares\n"
     if memFaultProcs:
-      PrintAndLog( f"{ len(memFaultProcs) } { "PROCESS" if len(memFaultProcs) == 1 else "PROCESSES" } ", end="", file=file )
-      PrintAndLog( f"WITH HIGH MEMORY USAGE (OVER {THRESHOLD_MEM} MB) DETECTED:", file=file )
-      LogProcessTable(memFaultProcs, file=file)
-      PrintAndLog("Suggestion:", file=file)
-      PrintAndLog("Turn off the processes that occupies too much memory\n", file=file)
+      resultCtnt += f"{ len(memFaultProcs) } { "PROCESS" if len(memFaultProcs) == 1 else "PROCESSES" }\n"
+      resultCtnt += f"WITH HIGH MEMORY USAGE (OVER {THRESHOLD_MEM} MB) DETECTED:\n"
+      resultCtnt += LogProcessTable(memFaultProcs)
+      resultCtnt += "Suggestion:\n"
+      resultCtnt += "Turn off the processes that occupies too much memory\n"
+      resultCtnt += "and release it to the other needed processes.\n"
     if runTooLongProcs:
-      PrintAndLog( f"{ len(runTooLongProcs) } { "PROCESS" if len(runTooLongProcs) == 1 else "PROCESSES" } ", end="", file=file )
-      PrintAndLog( f"WITH RUNTIME OVER {format_duration(THRESHOLD_RUNTIME).upper()} DETECTED:", file=file )
-      LogProcessTable(runTooLongProcs, file=file)
-      PrintAndLog("Suggestion:\nTurn off unused processes to prevent them from", end=" ", file=file)
-      PrintAndLog("occupying CPU and memory", file=file)
-      PrintAndLog("NOTE: Only close them if necessary.", end=" ", file=file)
-      PrintAndLog("Turning off necessary programs may result in system failure.\n", file=file)
+      resultCtnt += f"{ len(runTooLongProcs) } { "PROCESS" if len(runTooLongProcs) == 1 else "PROCESSES" } "
+      resultCtnt += f"WITH RUNTIME OVER {duration_readable_format(THRESHOLD_RUNTIME).upper()} DETECTED:\n"
+      resultCtnt += LogProcessTable(runTooLongProcs)
+      resultCtnt += "Suggestion:\nTurn off these unused processes to release CPU and memory\n"
+      resultCtnt += "resources to the other needed processes.\n"
+      resultCtnt += "NOTE: Only close them if necessary.\n"
+      resultCtnt += "Turning off indispensable programs may result in system failure.\n"
   else:
-    PrintAndLog( f"\nNo problematic processes found {recTime}", file=file )
+    resultCtnt += f"\nNo problematic processes found {recTime}\n"
+
+  return resultCtnt
 
 
 
 
 
-def monitor(procList):
+def monitor(procList, nSecs):
   """
   Monitors operating system usages such as CPU, memory, etc. for long-running processes
   """
@@ -367,8 +393,7 @@ def monitor(procList):
     now = curTime.timestamp()
 
     print( f"Attempting to record operating system processes at", end=" ")
-    print( f"{ curTime.strftime( "%H:%M:%S on %Y-%m-%d" ) }\n" )
-    print( "Monitoring system processes...." )
+    print( f"{ curTime.strftime( "%H:%M:%S on %Y-%m-%d" ) }\n\nMonitoring system processes...." )
 
     procs = [] # List of processes to monitor
  
@@ -393,9 +418,9 @@ def monitor(procList):
       print( "Done!", end=" " )
 
     # Display all the problems among the filtered, monitored processes
-    OS_monitoring_summary(
+    result = OS_monitoring_summary(
       curTime.strftime( "at %H:%M:%S on %Y-%m-%d" ),
-      procs, CPU_OUTAGE_PROCS, MEM_OUTAGE_PROCS, RUN_TOO_LONG_PROCS, LOG_FILE_NAME
+      procs, CPU_OUTAGE_PROCS, MEM_OUTAGE_PROCS, RUN_TOO_LONG_PROCS
     )
 
     # Clear all process list before the next monitoring
@@ -404,7 +429,9 @@ def monitor(procList):
     RUN_TOO_LONG_PROCS.clear()
     procs.clear()
 
-    time.sleep(INTERVAL)
+    PrintAndLog( result, file=LOG_FILE_NAME )
+
+    time.sleep(nSecs)
 
     PrintAndLog( "-" * 120, end="\n\n", file=LOG_FILE_NAME )
 
@@ -415,10 +442,15 @@ def monitor(procList):
 ### Entry point of this program. Similar to C++'s int main()
 if __name__ == "__main__":
   try:
+    clearScreen()
+    # Warning: setting it too low may result in more data written to a log file
+    #          and can consume too much storage space unnecessarily quickly.
+    INTERVAL = 59.4    # time gap between each monitoring, in seconds
+
     print( "Welcome to the system-monitoring application." )
     print( f"While monitoring, it records the result to a log file about every ", end="" )
-    print( f"{round(INTERVAL)} {"second" if INTERVAL == 1 else "seconds"}.\n" )
-    print( "Press Ctrl+C to close this application\n" )
+    print( f"{math.ceil(INTERVAL)} {"second" if math.ceil(INTERVAL) == 1 else "seconds"}.\n" )
+    print( "Press Ctrl+C to close this application.\n" )
 
     enterInput = input("For now, press Enter to activate the monitoring process.")
 
@@ -429,14 +461,16 @@ if __name__ == "__main__":
 
     # This process list is adjustable
     # and will monitor the processes you assigned.
+    # You can adjust what you wish to monitor
+    # by changing the process names below, though.
     procList = [
-      # "MicrosoftSecurityApp.exe",   # Microsoft security
+      "MicrosoftSecurityApp.exe",   # Microsoft security
       "OneDrive.exe",               # OneDrive cloud
       "SafeConnect.Entry.exe",      # McAfee Safe Connect
       "dwm.exe",                    # Desktop window manager
     ]
 
-    monitor(procList)
+    monitor(procList, INTERVAL)
   except KeyboardInterrupt:
     clearScreen()
     print( "Now shutting this application down...." )
